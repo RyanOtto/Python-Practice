f48f323a6ad2b43c82f9e59512e37fc8f2f7c199
Ryan Otto <rotto@drew.edu>
2016-10-31 22:08:49 -0400

    Created Hangman program
    Created Hangman program, created a text file with a list of words to
    play with.

---
+++ Hangman.py
@@ -0,0 +1,42 @@
+#Simulate a game of Hangman.
+import random
+textFileLines = open("Resources/hangmanwords.txt").readlines()
+randomLine = random.choice(textFileLines)
+hangmanWords = randomLine.split()
+currentWord = random.choice(hangmanWords).lower()
+gameFinished = False
+lettersGuessed = []
+wrongGuesses = 0
+userDisplayLine = []
+correctGuesses = 0
+
+print("You're playing hangman!  If you make 10 incorrect guesses, you lose.")
+for i in range(0, len(currentWord)):
+    userDisplayLine .append("_")
+while gameFinished is False:
+    print(" ".join(userDisplayLine))
+    guessedLetter = input("Guess a letter --> ").lower()
+
+    if guessedLetter in lettersGuessed:
+        print("You've already guessed  '", guessedLetter,"'")
+    if len(guessedLetter) > 1 or guessedLetter.isalpha() is False:
+        print("Invalid input.  Please enter a single letter as a guess.")
+    else:
+        if guessedLetter in currentWord:
+            for i in range(0, len(currentWord)):
+                if currentWord[i] == guessedLetter:
+                    userDisplayLine[i] = guessedLetter
+                    correctGuesses += 1
+            lettersGuessed.append(guessedLetter)
+            print("Guessed Letters: ", lettersGuessed)
+        else:
+            wrongGuesses += 1
+            print(guessedLetter, "is not in the word.  Incorrect guesses: ", wrongGuesses)
+            lettersGuessed.append(guessedLetter)
+            print("Guessed Letters: ", lettersGuessed)
+    if(wrongGuesses == 10):
+        print("You have lost the game.  The word was", currentWord)
+        gameFinished = True
+    if(correctGuesses == len(currentWord)):
+        print("You've won!  The word was", currentWord)
+        gameFinished = True
