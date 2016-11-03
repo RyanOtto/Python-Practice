917eb56ecd2b0ac5079e3887f075523e225f656e
Ryan Otto <rotto@drew.edu>
2016-11-03 09:18:23 -0400

    Replace User Input Checks
    User input while loops checking for valid input have been changed so
    that they use try:catch statements to check for valid input.  Input
    other than 2 numbers from the range of 0-2 should no longer be possible

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,5 +1,5 @@
 # Simulate a game of Tic Tac Toe
-#TODO: Don't let player type in letters
+#TODO: Don't let player choose a spot already filled
 #TODO: End game when entire board is filled and it's a draw, print message saying it's a draw
 
 # 0 means unoccupied space, 1 and 2 mean occupied by the players.
@@ -11,7 +11,6 @@ ticTacToeVisual = [['', '', ''],
                    ['', '', ''],
                    ['', '', '']]
 gameOutcome = ""
-gameFinished = False
 
 def drawboard():
         for i in range(3):
@@ -57,25 +56,36 @@ def isgamefinished():
     return gameFinished
 
 
-while isgamefinished() is False:
+while not isgamefinished():
     drawboard()
     playerOneChoice = [-1, -1]
     playerTwoChoice = [-1, -1]
+    while True:
+        try:
+            playerOneChoice = str(input("\nPlayer 1: Select a row and column, separated by a space (IE 1 1): ")).split(" ")
+            ticTacToeList[int(playerOneChoice[0])][int(playerOneChoice[1])] = 1
+            break
+        except ValueError:
+            print("Please enter a valid set of coordinates on the board from 0-2 for both column and row")
+        except IndexError:
+            print("Please enter a valid set of coordinates on the board from 0-2 for both column and row")
 
-    while int(playerOneChoice[0]) < 0 or int(playerOneChoice[0]) > 2 or int(playerOneChoice[1]) < 0 or int(playerOneChoice[1]) > 2:
-        print("Please enter a valid set of coordinates on the board from 0-2 for both column and row")
-        playerOneChoice = str(input("\nPlayer 1: Select a row and column, separated by a space (IE 1 1): ")).split(" ")
-    ticTacToeList[int(playerOneChoice[0])][int(playerOneChoice[1])] = 1
     if isgamefinished():
         drawboard()
         print("Player 1 has won!")
 
     if not isgamefinished():
         drawboard()
-        while int(playerTwoChoice[0]) < 0 or int(playerTwoChoice[0]) > 2 or int(playerTwoChoice[1]) < 0 or int(playerTwoChoice[1]) > 2:
-            print("Please enter a valid set of coordinates on the board from 0-2 for both column and row")
-            playerTwoChoice = str(input("\nPlayer 2: Select a row and column, separated by a space (IE 1 1): ")).split(" ")
-        ticTacToeList[int(playerTwoChoice[0])][int(playerTwoChoice[1])] = 2
+        while True:
+            try:
+                playerTwoChoice = str(input("\nPlayer 2: Select a row and column, separated by a space (IE 1 1): ")).split(" ")
+                ticTacToeList[int(playerTwoChoice[0])][int(playerTwoChoice[1])] = 2
+                break
+            except ValueError:
+                print("Please enter a valid set of coordinates on the board from 0-2 for both column and row")
+            except IndexError:
+                print("Please enter a valid set of coordinates on the board from 0-2 for both column and row")
+
         if isgamefinished():
             drawboard()
             print("Player 2 has won!")
