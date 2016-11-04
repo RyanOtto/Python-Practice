f77e1021ff6614a3435b55fd26b9aed3523d46fe
Ryan Otto <rotto@drew.edu>
2016-11-04 12:15:54 -0400

    TicTacToe now accounts for draw
    If the board is filled and no winner is declared, the game will now end
    in a draw.
    If a spot is already filled, player should no longer be able to pick
    that spot for their turn.

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,6 +1,4 @@
 # Simulate a game of Tic Tac Toe
-#TODO: Don't let player choose a spot already filled
-#TODO: End game when entire board is filled and it's a draw, print message saying it's a draw
 
 # 0 means unoccupied space, 1 and 2 mean occupied by the players.
 ticTacToeList = [[0, 0, 0],
@@ -40,52 +38,63 @@ def drawboard():
             print("|", ticTacToeVisual[i][0], "|  |", ticTacToeVisual[i][1], "|  |",ticTacToeVisual[i][2], "|")
 
 def isgamefinished():
-    gameFinished = False
+    gameFinished = 0  #0 means game not over, 1 means there's a winner, 2 means a draw
     for i in range(3):
         # Vertical win
         if ticTacToeList[0][i] == ticTacToeList[1][i] == ticTacToeList[2][i] != 0:
-            gameFinished = True
+            gameFinished = 1
         # Horizontal win
         elif ticTacToeList[i][0] == ticTacToeList[i][1] == ticTacToeList[i][2] != 0:
-            gameFinished = True
+            gameFinished = 1
         # Diagonal win
-        elif ticTacToeList[0][0] == ticTacToeList[1][1] == ticTacToeList[2][2] != 0 or ticTacToeList[0][2] == \
-                ticTacToeList[1][1] == ticTacToeList[2][0] != 0:
-            gameFinished = True
+        elif ticTacToeList[0][0] == ticTacToeList[1][1] == ticTacToeList[2][2] != 0 or ticTacToeList[0][2] == ticTacToeList[1][1] == ticTacToeList[2][0] != 0:
+            gameFinished = 1
+        # Draw
+        elif not any(0 in sublist for sublist in ticTacToeList):
+            gameFinished = 2
 
     return gameFinished
 
 
-while not isgamefinished():
+while isgamefinished() == 0:
     drawboard()
     playerOneChoice = [-1, -1]
     playerTwoChoice = [-1, -1]
     while True:
         try:
-            playerOneChoice = str(input("\nPlayer 1: Select a row and column, separated by a space (IE 1 1): ")).split(" ")
-            ticTacToeList[int(playerOneChoice[0])][int(playerOneChoice[1])] = 1
-            break
+            playerOneChoice = str(input("\nPlayer 1: Select an empty row and column, separated by a space (IE 1 1): ")).split(" ")
+            if ticTacToeList[int(playerOneChoice[0])][int(playerOneChoice[1])] == 0:
+                ticTacToeList[int(playerOneChoice[0])][int(playerOneChoice[1])] = 1
+                break
         except ValueError:
             print("Please enter a valid set of coordinates on the board from 0-2 for both column and row")
         except IndexError:
             print("Please enter a valid set of coordinates on the board from 0-2 for both column and row")
 
-    if isgamefinished():
+    if isgamefinished() == 1:
         drawboard()
         print("Player 1 has won!")
+    elif isgamefinished() == 2:
+        drawboard()
+        print("Draw!")
+            
 
-    if not isgamefinished():
+    if isgamefinished() == 0:
         drawboard()
         while True:
             try:
-                playerTwoChoice = str(input("\nPlayer 2: Select a row and column, separated by a space (IE 1 1): ")).split(" ")
-                ticTacToeList[int(playerTwoChoice[0])][int(playerTwoChoice[1])] = 2
-                break
+                playerTwoChoice = str(input("\nPlayer 2: Select an empty row and column, separated by a space (IE 1 1): ")).split(" ")
+                if ticTacToeList[int(playerTwoChoice[0])][int(playerTwoChoice[1])] == 0:
+                    ticTacToeList[int(playerTwoChoice[0])][int(playerTwoChoice[1])] = 2
+                    break
             except ValueError:
                 print("Please enter a valid set of coordinates on the board from 0-2 for both column and row")
             except IndexError:
                 print("Please enter a valid set of coordinates on the board from 0-2 for both column and row")
 
-        if isgamefinished():
+        if isgamefinished() == 1:
             drawboard()
             print("Player 2 has won!")
+        elif isgamefinished() == 2:
+            drawboard()
+            print("Draw!")
