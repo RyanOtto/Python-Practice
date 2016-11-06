75c98e6dd0631eea73c7b2e8edbc479be4d9fd9d
Ryan Otto <rotto@drew.edu>
2016-11-06 15:31:05 -0500

    Finished Commit
    Previous commit did not reflect final Fibonnaci program, and this commit
    should account for the complete program.

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,10 +1,13 @@
+# Prompts user for how many numbers in the Fibonacci sequence they want to see, and prints them
 def Fibonaccigenerator():
     fibonnaciInput = int(input("How many fibonacci numbers would you like to generate? "))
     firstFibNum = 0
     secondFibNum = 1
     storedFibNum = 0
     for i in range(fibonnaciInput):
+        print(secondFibNum, end=" ")
         storedFibNum = secondFibNum
         secondFibNum += firstFibNum
         firstFibNum = storedFibNum
         
+Fibonaccigenerator()
