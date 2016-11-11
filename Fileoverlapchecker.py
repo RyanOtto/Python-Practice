4661a1904d7ffadfc442d8bc68c40cd6eb6a3100
Ryan Otto <rotto@drew.edu>
2016-11-11 13:56:17 -0500

    Created Checkduplicates method
    * Moved code logic into a method so that it could be called on any two
    files
    * Added two text files meant to check for duplicates of varying types.

--- Fileoverlapchecker.py
+++ Fileoverlapchecker.py
@@ -1,14 +1,18 @@
-# Read two text files, and list the duplicate entries.  Also list the number of duplicates for each entry.
-# TODO: Put this logic in a method that works with any two files
-with open('Resources/happynumbers.txt', 'r') as open_file:
-    happyNumbers = open_file.read().split()
+# Read two text files, and list the duplicate entries.
+# TODO: Change variable names to be more universal
+def Checkduplicates(firstFile, secondFile):
+    with open(firstFile, 'r') as open_file:
+        firstFileContents = open_file.read().split()
+    
+    with open(secondFile, 'r') as open_file:
+        secondFileContents = open_file.read().split()
+    
+    overlapList = []
+    for i in range(len(secondFileContents)):
+        if secondFileContents[i] in firstFileContents:
+            if not secondFileContents[i] in overlapList:
+                overlapList.append(secondFileContents[i])
+    return overlapList
 
-with open('Resources/primenumbers.txt', 'r') as open_file:
-    primeNumbers = open_file.read().split()
-
-overlapList = []
-for i in range(len(primeNumbers)):
-    if primeNumbers[i] in happyNumbers:
-        overlapList.append(primeNumbers[i])
-
-print("Duplicates: ", ", ".join(str(i) for i in overlapList))
+print("Duplicates: ", Checkduplicates('Resources/happynumbers.txt', 'Resources/primenumbers.txt'))
+print("Duplicates: ", Checkduplicates('Resources/duptextfile1.txt', 'Resources/duptextfile2.txt'))
