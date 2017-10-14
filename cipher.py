b0ec96a5067cf0a70338c2637e44d1b1cecb08dd
Ryan Otto <rotto@drew.edu>
2017-10-14 11:32:08 -0400

    Add cipher.py
    * Will take a string and return the new string after it has been run through a cipher.

---
+++ cipher.py
@@ -0,0 +1,25 @@
+#Given a string and a key, run that string through a cipher
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+word= ''
+
+#Prompt for a string or char
+word = input("Enter a string:")
+
+#Prompt for a number until we get one
+while 1:
+    try:
+        key = int(input("Enter a key:"))
+    except ValueError:
+        print("Not a valid key")
+    else:
+        break
+
+#Print out the new string after running through the cipher
+for letter in word.lower():
+    if letter != ' ':
+        try:
+            print(alphabet[alphabet.index(letter)+key], end="")
+        except:
+            print(alphabet[alphabet.index(letter)+key-26], end="")
+    else:
+        print(' ',end="")
